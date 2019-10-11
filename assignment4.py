77f0d3abe5d9b60c490d51436644e8562790b653
jakobvoi <56079848+jakobvoi@users.noreply.github.com>
2019-10-11 14:07:49 +0200

    start infer()
    start

--- assignment4.py
+++ assignment4.py
@@ -1,17 +1,38 @@
+#  R  S
+#  |\ |
+#  | \|
+#  W  H
+
+
+
 class Node:
     def __init__(self, name, p_table ):
         self.name = name
         self.p_table = p_table
-        self.state
+        self.state = 0
         # number of times state = 0, and state = 1:
-        self.distrobution
-        self.parents
-        self.children
+        self.distrobution = {0:0,1:0}
+        self.parents = {}
+        self.children ={}
 
-node_R = Node(R, )
+R_node = Node('R', None)
+S_node = Node('S', None)
+W_node = Node('W', None)
+H_node = Node('H', None)
 
+nodes = {'R':R_node, 'S':S_node, 'W':W_node, 'H':H_node}
+
+
+given_states_example = {'H':1,'W':1}
+p_x_state_example = {'R':1}
 
 def infer_probability(p_x_state : dict, given_states: dict):
     #set observered nodes
+    for n in given_states.keys():
 
-    #
+
+
+
+
+
+infer_probability(given_states_example, p_x_state_example)
