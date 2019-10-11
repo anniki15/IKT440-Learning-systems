f25c2a21f4ccd21f8f1d60f43bc00af19849a543
jakobvoi <56079848+jakobvoi@users.noreply.github.com>
2019-10-11 13:27:59 +0200

    Create assignment4.py

---
+++ assignment4.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, name, p_table ):
+        self.name = name
+        self.p_table = p_table
+        self.state
+        # number of times state = 0, and state = 1:
+        self.distrobution
+        self.parents
+        self.children
+
+node_R = Node(R, )
+
+
+def infer_probability(p_x_state : dict, given_states: dict):
+    #set observered nodes
+
+    #
