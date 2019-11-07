0ab261e6c0415081c9ab8f8a441aafc009d96d03
jakobvoi <56079848+jakobvoi@users.noreply.github.com>
2019-11-07 09:05:33 +0100

    The ant walks!

--- aco.py
+++ aco.py
@@ -16,14 +16,39 @@ class Node:
         self.name = name
         self.coordinate_x = coordinates[0]
         self.coordinate_y = coordinates[1]
+        self.edges = {}
 
     def distance_to(self, toNode):
         return math.sqrt((self.coordinate_x - toNode.coordinate_x)**2 + (self.coordinate_y - toNode.coordinate_y)**2)
+
+    def rouletteWheel(self, visitedEdges, startNode, endNode):
+        visitedNodes = [oneEdge.toNode for oneEdge in visitedEdges.values()]
+        viableEdges = [oneEdge for oneEdge in self.edges.values()
+                       if not oneEdge.toNode in visitedNodes and oneEdge.toNode != startNode and oneEdge.toNode != endNode]
+        # If all nodes are visited, get back to the starting node
+        if not viableEdges:
+            viableEdges = [oneEdge for oneEdge in self.edges if not oneEdge.toNode in visitedNodes]
+        allPheromones = sum([oneEdge.pheromones for oneEdge in viableEdges])
+        num = random.uniform(0, allPheromones)
+        s = 0
+        i = 0
+        selectedEdge = viableEdges[0]
+        while (s <= num):
+            selectedEdge = viableEdges[i]
+            s += selectedEdge.pheromones
+            i += 1
+        return selectedEdge
+
+    def __repr__(self):
+        return self.name
+
+
 
 class Edge:
     def __init__(self, fromNode, toNode):
         self.fromNode = fromNode
         self.toNode = toNode
+        self.key = str(fromNode.name + toNode.name)
         self.cost = fromNode.distance_to(toNode)
         self.pheromones = 1
 
@@ -43,45 +68,19 @@ class ANT:
     def walk(self,  startNode: Node, endNode: Node, nodesToVisit: int):
         """
         Walks from 'startNode', visits an amount of nodes equal to 'nodesToVisit', then walks to endNode
+        The graph defines the network of nodes in witch the ant will walk.
         """
         currentNode = startNode
-
-
         while (len(self.visitedEdges) < nodesToVisit ):
-            nextNode = currentNode.roluetteWheelSimple()
+            #Choose next edge to travel
+            currentEdge = currentNode.rouletteWheel(self.visitedEdges, startNode, endNode)
             currentNode = currentEdge.toNode
-            self.visitedEdges.append(currentEdge)
+            self.visitedEdges[currentEdge.key] = currentEdge
 
-    def pheromonesWitoutMMAS(self):
-        currentCost = getSum(self.visitedEdges)
-        # Score
-        score = 10 ** (1 - float(currentCost) / MAXCOST)  # 1
-        for oneEdge in self.visitedEdges:
-            oneEdge.pheromones += score
+        last_edge_key = str(currentNode.name + endNode.name)
+        n = allEdges[last_edge_key]
 
-    def pheromones(self):
-        currentCost = getSum(self.visitedEdges)
-        # Score
-        score = 10 ** (1 - float(currentCost) / MAXCOST)  # 1
-        global bestScore
-        global bestSolution
-        if (score > bestScore):
-            bestScore = score
-            bestSolution = self.visitedEdges
 
-        for oneEdge in bestSolution:  # self.visitedEdges:
-            oneEdge.pheromones += score
-
-### NON-CLASS FUNCTIONS ###
-def evaporation(egdes):
-    for edge in egdes:
-        edge.pheromones *= 0.99
-
-def checkAllEdges(edges):
-    for edge in edges:
-        edge.checkPheromones()
-
-def distribute_pheromones(edges)
 
 ### HARD KODA TING ###
 nodes_example = {
@@ -98,8 +97,33 @@ nodes_example = {
 }
 
 edges_example = {
-    'node1node2': Edge(nodes_example('node1'), nodes_example('node2'))
+    'node1node2': Edge(nodes_example['node1'], nodes_example['node2'])
 }
 ### "Main" - aka. run the algorithm ###
+
+allNodes = nodes_example
+allEdges = {}
+
+#Assign edges between all nodes
+
+def run(n):
+    for fromNode in allNodes.values():
+        for toNode in allNodes.values():
+            if toNode != fromNode:
+                eddie = Edge(fromNode, toNode)
+                key = str(fromNode.name + toNode.name)
+                print(key)
+                fromNode.edges[key] = eddie
+                allEdges[key] = eddie
+
+    for i in range(10):
+        annie = ANT()
+        annie.walk(nodes_example['node1'], nodes_example['node10'], n)
+
+run(8)
+
+
+
+
 
 
